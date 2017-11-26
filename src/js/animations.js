7c4a14ef94bb94f09195ad89c240826425d079aa
ChaseMeImFamous <kevin.brillion@gmail.com>
2017-11-26 22:18:35 +0100

    :construction: add sixth and seventh section + beginning slider sixth

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,8 +1,16 @@
 export class Animations {
-  
+
 }
 
   let SVGAnimation = new Vivus('svg--animated');
+
+  let second = new Waypoint ({
+    element: document.querySelector('#second__waypoints'),
+    handler: function(direction) {
+      SVGAnimation.play();
+    },
+    offset: '10%'
+  })
 
   let third = new Waypoint ({
     element: document.querySelector('#third__waypoints'),
@@ -12,10 +20,10 @@ export class Animations {
     offset: '10%'
   })
 
-  let second = new Waypoint ({
-    element: document.querySelector('#second__waypoints'),
-    handler: function(direction) {
-      SVGAnimation.play();
-    },
-    offset: '10%'
-  })
+  let slider = tns({
+  container: '.sixth__slider',
+  navContainer: ".sixth__thumbnails",
+  items: 1,
+  mouseDrag: true,
+  controls: false
+  });
